b2fa4948f6e7b33b90563444067cb792fc6b2607
Bradley Golden <golden.bradley@gmail.com>
2015-12-16 10:17:37 -0800

    Added DataTier class

---
+++ ChicagoEmployeeSalaries/DataTier.h
@@ -0,0 +1,13 @@
+//
+//  DataTier.h
+//  ChicagoEmployeeSalaries
+//
+//  Created by Bradley Golden on 12/16/15.
+//  Copyright © 2015 Oliver San Juan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataTier : NSObject
+
+@end

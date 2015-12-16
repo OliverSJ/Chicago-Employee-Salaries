af2eae0e67815ea9f8c724c60b151ab49e37857c
Oliver San Juan <osanju2@gmail.com>
2015-12-16 11:21:10 -0600

    Initial Commit

---
+++ ChicagoEmployeeSalaries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChicagoEmployeeSalaries
+//
+//  Created by Oliver San Juan on 12/16/15.
+//  Copyright © 2015 Oliver San Juan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChicagoEmployeeSalaries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChicagoEmployeeSalaries
+//
+//  Created by Oliver San Juan on 12/16/15.
+//  Copyright © 2015 Oliver San Juan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

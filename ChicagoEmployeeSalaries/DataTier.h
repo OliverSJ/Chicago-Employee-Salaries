ca04c5c433cb2725c23b4939d0d3d0c67a4bb2d9
Bradley Golden <golden.bradley@gmail.com>
2015-12-16 10:48:17 -0800

    still working on data tier

--- ChicagoEmployeeSalaries/DataTier.h
+++ ChicagoEmployeeSalaries/DataTier.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
 
 @interface DataTier : NSObject
+
+@property (nonatomic, strong) NSString *documentsDirectory;
+@property (nonatomic, strong) NSString *databaseFilename;
+@property (nonatomic, strong) NSMutableArray *arrResults;
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRows;
+@property (nonatomic) long long lastInsertedRowID;
+
+-(instancetype)initWithDatabaseFilename:(NSString*)dbFilename;
+-(void)copyDatabaseIntoDocumentsDirectory;
+-(void)runQuery:(const char *)query isQueryExecutable:(BOOL)queryExecutable;
 
 @end

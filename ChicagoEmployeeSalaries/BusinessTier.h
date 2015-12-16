915a14f75fd1a7f0e91aeacf9507639affbb870f
Oliver San Juan <osanju2@gmail.com>
2015-12-16 12:07:51 -0600

    Added Business Tier classes

---
+++ ChicagoEmployeeSalaries/BusinessTier.h
@@ -0,0 +1,13 @@
+//
+//  BusinessTier.h
+//  ChicagoEmployeeSalaries
+//
+//  Created by Oliver San Juan on 12/16/15.
+//  Copyright © 2015 Oliver San Juan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BusinessTier : NSObject
+
+@end
---
+++ ChicagoEmployeeSalaries/BusinessTier.m
@@ -0,0 +1,13 @@
+//
+//  BusinessTier.m
+//  ChicagoEmployeeSalaries
+//
+//  Created by Oliver San Juan on 12/16/15.
+//  Copyright © 2015 Oliver San Juan. All rights reserved.
+//
+
+#import "BusinessTier.h"
+
+@implementation BusinessTier
+
+@end

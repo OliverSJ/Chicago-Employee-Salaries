f5788d16bda9a8dfd6f11007880e5837f9be7ee8
Oliver San Juan <osanju2@gmail.com>
2015-12-18 00:13:27 -0600

    Made changes to GUI. Some features are now working

--- ChicagoEmployeeSalaries/ViewController.h
+++ ChicagoEmployeeSalaries/ViewController.h
@@ -10,6 +10,18 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *positionLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *departmentLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *salaryLabel;
+
+@property (strong, nonatomic) IBOutlet UITextField *firstNameTextField;
+
+@property (strong, nonatomic) IBOutlet UITextField *lastNameTextField;
+
 
 @end
 
--- ChicagoEmployeeSalaries/ViewController.m
+++ ChicagoEmployeeSalaries/ViewController.m
@@ -17,11 +17,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //When launching the app for the first time, these labels should be hidden
+    _nameLabel.hidden = YES;
+    _positionLabel.hidden = YES;
+    _departmentLabel.hidden = YES;
+    _salaryLabel.hidden = YES;
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)searchButton:(UIButton *)sender {
+    
+    /* TODO:
+     
+     Implement actions for when the user hits the search button.  
+     It should take the data from the two text fields and pass that to the Data Tier classes
+     
+     */
+    
+    //Grab the information from the text fields
+    NSString *firstName = _firstNameTextField.text;
+    NSString *lastName = _lastNameTextField.text;
+    
+    //TODO:
+    //Check to see if either field is empty
+    
+    //TODO:
+    //Launch the SQLite query
+    
+    
+    //NSString *fullName = [NSString stringWithFormat:@"%@ %@",firstName, lastName];
+    //Unhide the labels and then set their texts
+    _nameLabel.hidden = NO;
+   // _nameLabel.text = fullName;
+    
+    
+    
+    
+}
+
+
+
+
 
 @end

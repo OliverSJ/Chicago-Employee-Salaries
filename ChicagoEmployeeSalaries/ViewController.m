e93fa2df2efcb4d24cb1bb269589e08a44b6c601
Bradley Golden <golden.bradley@gmail.com>
2015-12-18 08:05:25 -0800

    restructured gui

--- ChicagoEmployeeSalaries/ViewController.m
+++ ChicagoEmployeeSalaries/ViewController.m
@@ -10,13 +10,41 @@
 
 @interface ViewController ()
 
+@property NSArray *departments;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *departmentTextField;
+
 @end
 
 @implementation ViewController
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    // user has pressed enter on the name text field
+    if (textField == self.nameTextField) {
+        [self.departmentTextField becomeFirstResponder];
+    }
+    else {
+        [textField resignFirstResponder];
+    }
+    // DO SEARCH HERE
+    
+    return YES;
+}
+
+- (IBAction)performSearch:(id)sender {
+    [self.nameTextField resignFirstResponder];
+    [self.departmentTextField resignFirstResponder];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES]; // force all text fields to end editing
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.departments = @[@"POLICE", @"MAYOR'S OFFICE", @"FONTANO'S", @"LAGUNITAS"];
 }
 
 - (void)didReceiveMemoryWarning {
